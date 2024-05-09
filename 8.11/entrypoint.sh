43310ec49943bd934345a0a582408ad5b0a06491
pritamdas99 <pritam@appscode.com>
2024-05-09 18:01:36 +0600

    Fix symbolic link issue.
    Signed-off-by: pritamdas99 <pritam@appscode.com>

--- 8.11/entrypoint.sh
+++ 8.11/entrypoint.sh
@@ -4,8 +4,11 @@ set -eo pipefail
 set -x
 
 AUTHFILE="/opt/solr/basicauth.properties"
-sed -i "s/SOLR_USER/$SOLR_USER/g" $AUTHFILE
-sed -i "s/SOLR_PASSWORD/$SOLR_PASSWORD/g" $AUTHFILE
+
+if [[ -f $AUTHFILE ]] && [[ $SOLR_USER != "" ]] && [[ $SOLR_PASSWORD != "" ]]; then
+    sed -i "s/SOLR_USER/$SOLR_USER/g" $AUTHFILE
+    sed -i "s/SOLR_PASSWORD/$SOLR_PASSWORD/g" $AUTHFILE
+fi
 
 docker-entrypoint.sh solr-foreground & 
 ./metrics-exporter.sh
