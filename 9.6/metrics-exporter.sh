d7eee4c50f8fb7a54d49976fea922075e0dfcb20
pritamdas99 <pritam@appscode.com>
2024-09-09 17:08:44 +0600

    Update curl command to ensure server running
    Signed-off-by: pritamdas99 <pritam@appscode.com>

--- 9.6/metrics-exporter.sh
+++ 9.6/metrics-exporter.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+openssl pkcs12 -in /var/solr/etc/keystore.p12 -out /var/solr/ssl.pem -legacy  -password pass:$SOLR_SSL_KEY_STORE_PASSWORD -nodes
 while true; do
-    RESULT=$(curl -k -s -o /dev/null -I -w '%{http_code}' -u "${SOLR_USER}:${SOLR_PASSWORD}" ${CONNECTION_SCHEME}://${CLUSTER_NAME}.${POD_NAMESPACE}.svc.cluster.local:8983/solr/admin/cores?action=STATUS)
+    RESULT=$(curl -E /var/solr/ssl.pem:$SOLR_SSL_KEY_STORE_PASSWORD --cacert /var/solr/ssl.pem -s -o /dev/null -I -w '%{http_code}' -u "${SOLR_USER}:${SOLR_PASSWORD}" ${CONNECTION_SCHEME}://${CLUSTER_NAME}.${POD_NAMESPACE}.svc.cluster.local:8983/solr/admin/cores?action=STATUS)
     if [ "$RESULT" -eq '200' ]; then
         break
     fi
