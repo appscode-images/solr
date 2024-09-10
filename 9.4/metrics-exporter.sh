6a36bf306660e6ec3cfc0b2ea4036f4cb13d5b25
pritamdas99 <pritam@appscode.com>
2024-09-10 11:55:06 +0600

    Update curl command to configure tls

--- 9.4/metrics-exporter.sh
+++ 9.4/metrics-exporter.sh
@@ -2,7 +2,12 @@
 
 openssl pkcs12 -in /var/solr/etc/keystore.p12 -out /var/solr/ssl.pem -legacy  -password pass:$SOLR_SSL_KEY_STORE_PASSWORD -nodes
 while true; do
-    RESULT=$(curl -E /var/solr/ssl.pem:$SOLR_SSL_KEY_STORE_PASSWORD --cacert /var/solr/ssl.pem -s -o /dev/null -I -w '%{http_code}' -u "${SOLR_USER}:${SOLR_PASSWORD}" ${CONNECTION_SCHEME}://${CLUSTER_NAME}.${POD_NAMESPACE}.svc.cluster.local:8983/solr/admin/cores?action=STATUS)
+    RESULT="000"
+    if [[ $SOLR_SSL_ENABLED == "true" ]]; then
+        RESULT=$(curl -E /var/solr/ssl.pem:$SOLR_SSL_KEY_STORE_PASSWORD --cacert /var/solr/ssl.pem -s -o /dev/null -I -w '%{http_code}' -u "${SOLR_USER}:${SOLR_PASSWORD}" ${CONNECTION_SCHEME}://${CLUSTER_NAME}.${POD_NAMESPACE}.svc.cluster.local:8983/solr/admin/cores?action=STATUS)
+    else
+        RESULT=$(curl -s -o /dev/null -I -w '%{http_code}' -u "${SOLR_USER}:${SOLR_PASSWORD}" ${CONNECTION_SCHEME}://${CLUSTER_NAME}.${POD_NAMESPACE}.svc.cluster.local:8983/solr/admin/cores?action=STATUS)
+    fi
     if [ "$RESULT" -eq '200' ]; then
         break
     fi
